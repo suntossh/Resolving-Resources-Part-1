d17bad7f990850cdef6c68a9a4adbafcf4715e2b
Santoshkumar Pandey <[suntossh.numerouno@gmail.com]>
2015-06-09 10:32:50 +0530

    First Commit

---
+++ src/main/java/com/suntossh/spring/Application.java
@@ -0,0 +1,29 @@
+package com.suntossh.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		final ApplicationContext context = new ClassPathXmlApplicationContext(
+				"application-config.xml");
+
+		for (int i = 0; i < 1000; i++) {
+			Thread t = new Thread(new Runnable() {
+
+				public void run() {
+					for (int i = 0; i < 1000; i++) {
+						MyResourceReader myResourceReader = context.getBean(
+								"myResourceReader", MyResourceReader.class);
+						//myResourceReader.print();
+						System.out.println(myResourceReader);
+					}
+				}
+			});
+			t.start();
+		}
+
+	}
+}
---
+++ src/main/java/com/suntossh/spring/MyResourceReader.java
@@ -0,0 +1,45 @@
+package com.suntossh.spring;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Scope(value="prototype")
+@Component()
+public class MyResourceReader {
+	
+	@Autowired
+	@Value("http://timesofindia.indiatimes.com/")
+	private Resource resource;
+	
+	public void print(){
+		BufferedReader bufferedReader = null;
+		try {
+			bufferedReader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+			for(String line= bufferedReader.readLine(); line != null; line = bufferedReader.readLine()){
+				System.out.println(line);
+				break;
+			}
+			
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+				try {
+					bufferedReader.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		
+	}
+}
